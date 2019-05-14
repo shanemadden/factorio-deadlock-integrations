952d3d3d5b69650a125b8e0f73a932a42b52efa8
Shane Madden <shanemadden@fake.email>
2019-05-13 19:11:39 -0600

    Update belt speed ratio with base mod, fix bob settings split, add doublespeed+ultibelts support

--- data-updates.lua
+++ data-updates.lua
@@ -2,11 +2,7 @@ if mods["xander-mod"] then
 	require("prototypes.xander")
 end
 if mods["boblogistics"] then
-	if settings.startup["deadlock-integrations-new-bob-recipes"].value then
-		require("prototypes.bob-new")
-	else
-		require("prototypes.bob")
-	end
+	require("prototypes.bob")
 end
 if mods["FactorioExtended-Plus-Transport"] then
 	require("prototypes.fextendedplus")
