317eecb6fd463b38ee7b74d324c84d0f308a6935
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-05-01 00:02:02 +0200

    Update to reflect new FactorioExtended changes

--- prototypes/fextended.lua
+++ prototypes/fextended.lua
@@ -1,80 +1,123 @@
--- Factorio-Extended
-deadlock.add_tier({
-	transport_belt      = "blistering-transport-belt",
-	colour              = {r=10, g=225, b=25},
-	underground_belt    = "blistering-transport-belt-to-ground",
-	splitter            = "blistering-splitter",
-	technology          = "logistics-4",
-	order               = "d",
-	loader_ingredients  = {
-		{"express-transport-belt-loader",1},
-		{"iron-gear-wheel",20},
-		{amount = 40,name = "lubricant",type = "fluid"},
-	},
-	loader_category     = "crafting-with-fluid",
-	beltbox_ingredients = {
-		{"express-transport-belt-beltbox",1},
-		{"iron-plate",40},
-		{"iron-gear-wheel",40},
-		{"processing-unit",5},
-	},
-	beltbox_technology  = "deadlock-stacking-4",
-})
-if data.raw.technology["deadlock-stacking-4"] then
-	local found = false
-	for _, techname in ipairs(data.raw.technology["deadlock-stacking-4"].prerequisites) do
-		if techname == "deadlock-stacking-3" then
-			found = true
-			break
-		end
-	end
-	if not found then
-		table.insert(data.raw.technology["deadlock-stacking-4"].prerequisites, "deadlock-stacking-3")
-	end
-end
-if data.raw.furnace["blistering-transport-belt-beltbox"] then
-	data.raw.furnace["express-transport-belt-beltbox"].next_upgrade = "blistering-transport-belt-beltbox"
-end
-if data.raw["loader-1x1"]["blistering-transport-belt-loader"] then
-	data.raw["loader-1x1"]["express-transport-belt-loader"].next_upgrade = "blistering-transport-belt-loader"
+local function add_technology_prerequisite(technology, prerequisite)
+    for _, techname in ipairs(technology.prerequisites) do
+        if techname == prerequisite then return end
+    end
+    table.insert(technology.prerequisites, prerequisite)
 end
 
+-- Factorio-Extended
+
+-- [Stainless steel tech]
+deadlock.add_stack("stainless-steel", nil, "stainless-steel", 32)
 deadlock.add_tier({
-	transport_belt      = "furious-transport-belt",
-	colour              = {r=10, g=25, b=225},
-	underground_belt    = "furious-transport-belt-to-ground",
-	splitter            = "furious-splitter",
-	technology          = "logistics-5",
-	order               = "e",
-	loader_ingredients  = {
-		{"blistering-transport-belt-loader",1},
-		{"iron-gear-wheel",20},
-		{amount = 80,name = "lubricant",type = "fluid"}
-	},
-	loader_category     = "crafting-with-fluid",
-	beltbox_ingredients = {
-		{"blistering-transport-belt-beltbox",1},
-		{"iron-plate",50},
-		{"iron-gear-wheel",50},
-		{"processing-unit",20}
-	},
-	beltbox_technology = "deadlock-stacking-5",
+    transport_belt = "transport-belt-1",
+    colour = {r = 169, g = 118, b = 21},
+    underground_belt = "underground-belt-1",
+    splitter = "splitter-1",
+    technology = "stainless-steel-tech",
+    order = "d",
+    loader_ingredients = {
+        {"express-transport-belt-loader", 1}, {"stainless-steel", 40},
+        {"iron-gear-wheel", 20},
+        {amount = 40, name = "lubricant", type = "fluid"}
+    },
+    loader_category = "crafting-with-fluid",
+    beltbox_ingredients = {
+        {"express-transport-belt-beltbox", 1}, {"stainless-steel", 40},
+        {"iron-gear-wheel", 40}, {"processing-unit", 5}
+    },
+    beltbox_technology = "deadlock-stacking-4"
 })
+
+if data.raw.technology["deadlock-stacking-4"] then
+    add_technology_prerequisite(data.raw.technology["deadlock-stacking-4"],
+                                "stainless-steel-tech")
+    add_technology_prerequisite(data.raw.technology["deadlock-stacking-4"],
+                                "deadlock-stacking-3")
+end
+
+if data.raw.furnace["transport-belt-1-beltbox"] then
+    data.raw.furnace["express-transport-belt-beltbox"].next_upgrade =
+        "transport-belt-1-beltbox"
+end
+if data.raw["loader-1x1"]["transport-belt-1-loader"] then
+    data.raw["loader-1x1"]["express-transport-belt-loader"].next_upgrade =
+        "transport-belt-1-loader"
+end
+
+-- [Titanium tech]
+deadlock.add_stack("titanium-ore", nil, "titanium-processing", 32)
+deadlock.add_stack("titanium-rod", nil, "titanium-processing", 32)
+deadlock.add_tier({
+    transport_belt = "transport-belt-2",
+    colour = {r = 10, g = 225, b = 25},
+    underground_belt = "underground-belt-2",
+    splitter = "splitter-2",
+    technology = "titanium-tech",
+    order = "d",
+    loader_ingredients = {
+        {"transport-belt-1-loader", 1}, {"titanium-rod", 40},
+        {"iron-gear-wheel", 20},
+        {amount = 40, name = "lubricant", type = "fluid"}
+    },
+    loader_category = "crafting-with-fluid",
+    beltbox_ingredients = {
+        {"transport-belt-1-beltbox", 1}, {"titanium-rod", 40},
+        {"iron-gear-wheel", 40}, {"processing-unit", 5}
+    },
+    beltbox_technology = "deadlock-stacking-5"
+})
+
 if data.raw.technology["deadlock-stacking-5"] then
-	local found = false
-	for _, techname in ipairs(data.raw.technology["deadlock-stacking-5"].prerequisites) do
-		if techname == "deadlock-stacking-4" then
-			found = true
-			break
-		end
-	end
-	if not found then
-		table.insert(data.raw.technology["deadlock-stacking-5"].prerequisites, "deadlock-stacking-4")
-	end
+    add_technology_prerequisite(data.raw.technology["deadlock-stacking-5"],
+                                "titanium-tech")
+    add_technology_prerequisite(data.raw.technology["deadlock-stacking-5"],
+                                "deadlock-stacking-4")
 end
-if data.raw.furnace["furious-transport-belt-beltbox"] then
-	data.raw.furnace["blistering-transport-belt-beltbox"].next_upgrade = "furious-transport-belt-beltbox"
+
+if data.raw.furnace["transport-belt-2-beltbox"] then
+    data.raw.furnace["transport-belt-1-beltbox"].next_upgrade =
+        "transport-belt-2-beltbox"
 end
-if data.raw["loader-1x1"]["furious-transport-belt-loader"] then
-	data.raw["loader-1x1"]["blistering-transport-belt-loader"].next_upgrade = "furious-transport-belt-loader"
+if data.raw["loader-1x1"]["transport-belt-2-loader"] then
+    data.raw["loader-1x1"]["transport-belt-1-loader"].next_upgrade =
+        "transport-belt-2-loader"
+end
+
+-- [Graphene tech]
+deadlock.add_stack("graphene", nil, "graphene-processing", 32)
+deadlock.add_tier({
+    transport_belt = "transport-belt-3",
+    colour = {r = 225, g = 25, b = 225},
+    underground_belt = "underground-belt-3",
+    splitter = "splitter-3",
+    technology = "graphene-tech",
+    order = "d",
+    loader_ingredients = {
+        {"transport-belt-2-loader", 1}, {"graphene", 40},
+        {"iron-gear-wheel", 20},
+        {amount = 40, name = "lubricant", type = "fluid"}
+    },
+    loader_category = "crafting-with-fluid",
+    beltbox_ingredients = {
+        {"transport-belt-1-beltbox", 1}, {"graphene", 40},
+        {"iron-gear-wheel", 40}, {"processing-unit", 5}
+    },
+    beltbox_technology = "deadlock-stacking-6"
+})
+
+if data.raw.technology["deadlock-stacking-6"] then
+    add_technology_prerequisite(data.raw.technology["deadlock-stacking-6"],
+                                "graphene-tech")
+    add_technology_prerequisite(data.raw.technology["deadlock-stacking-6"],
+                                "deadlock-stacking-5")
+end
+
+if data.raw.furnace["transport-belt-3-beltbox"] then
+    data.raw.furnace["transport-belt-2-beltbox"].next_upgrade =
+        "transport-belt-3-beltbox"
+end
+if data.raw["loader-1x1"]["transport-belt-3-loader"] then
+    data.raw["loader-1x1"]["transport-belt-2-loader"].next_upgrade =
+        "transport-belt-3-loader"
 end
